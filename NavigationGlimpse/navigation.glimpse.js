200d30999bc881938608e56b675c64fd181cfe77
Graham Mendick <graham.mendick@gmail.com>
2013-08-10 12:47:42 +0100

    Pubsub event setup
    Register for prerender event, store data and publish render event

--- NavigationGlimpse/navigation.glimpse.js
+++ NavigationGlimpse/navigation.glimpse.js
@@ -1,1 +1,20 @@
-﻿
+﻿(function ($, pubsub, tab, render) {
+    var dialogs = {};
+    (function(){
+        var render = function () {
+        };
+        pubsub.subscribe('trigger.navigation.event.render', render);
+    })();
+    (function () {
+        var init = function () {
+            pubsub.publish('trigger.navigation.event.render');
+        },
+        prerender = function (args) {
+            dialogs.data = args.pluginData.data;
+            args.pluginData.data = 'Loading data, please wait...';
+            pubsub.publishAsync('trigger.navigation.init');
+        };
+        pubsub.subscribe('trigger.navigation.init', init);
+        pubsub.subscribe('action.panel.rendering.navigation_glimpse', prerender);
+    })();
+})(jQueryGlimpse, glimpse.pubsub, glimpse.tab, glimpse.render);
