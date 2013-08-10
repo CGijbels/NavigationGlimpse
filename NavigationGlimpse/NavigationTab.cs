ada4e39cb03123f58c6f45c572e0fbe2fbfe3bc9
Graham Mendick <graham.mendick@gmail.com>
2013-08-10 11:51:24 +0100

    Base plugin and sample projects
    Basic structure for the Navigation Glimpse plugin together with a sample
    web project for testing

---
+++ NavigationGlimpse/NavigationTab.cs
@@ -0,0 +1,26 @@
+﻿using Glimpse.Core.Extensibility;
+using Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationGlimpse
+{
+    public class NavigationTab : TabBase
+    {
+        public override object GetData(ITabContext context)
+        {
+            return StateInfoConfig.Dialogs;
+        }
+
+        public override string Name
+        {
+            get 
+            { 
+                return "Navigation";
+            }
+        }
+    }
+}
