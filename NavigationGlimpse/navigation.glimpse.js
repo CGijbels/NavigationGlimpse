1652e93e093882db64d49fb10dfac7a76a12efc7
Graham Mendick <graham.mendick@gmail.com>
2013-08-10 13:58:47 +0100

    Add the canvas to the navigation tab panel

--- NavigationGlimpse/navigation.glimpse.js
+++ NavigationGlimpse/navigation.glimpse.js
@@ -1,20 +1,36 @@
 ﻿(function ($, pubsub, tab, render) {
-    var dialogs = {};
+    var navigation = {};
+
+    (function () {
+        var setup = function () {
+            navigation.scope.html('<canvas id="navigation-glimpse"></canvas>');
+        };
+        pubsub.subscribe('trigger.navigation.shell.init', setup);
+    })();
+
     (function(){
         var render = function () {
         };
         pubsub.subscribe('trigger.navigation.event.render', render);
     })();
+
     (function () {
         var init = function () {
+            pubsub.publish('trigger.navigation.shell.init');
             pubsub.publish('trigger.navigation.event.render');
         },
         prerender = function (args) {
-            dialogs.data = args.pluginData.data;
+            args.pluginData._data = args.pluginData.data;
+            args.pluginData.data = 'Loading data, please wait...';
+        },
+        postrender = function (args) {
+            navigation.data = args.pluginData._data;
+            navigation.scope = args.panel;
             args.pluginData.data = 'Loading data, please wait...';
             pubsub.publishAsync('trigger.navigation.init');
         };
         pubsub.subscribe('trigger.navigation.init', init);
         pubsub.subscribe('action.panel.rendering.navigation_glimpse', prerender);
+        pubsub.subscribe('action.panel.rendered.navigation_glimpse', postrender);
     })();
 })(jQueryGlimpse, glimpse.pubsub, glimpse.tab, glimpse.render);
