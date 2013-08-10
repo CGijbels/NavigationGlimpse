f9c1c8dc06f8f64442e6f3e7b10428c066b2eb4c
Graham Mendick <graham.mendick@gmail.com>
2013-08-10 11:56:49 +0100

    Include navigation.glimpse.js in page
    Client and Resource classes added to register custom javascript with the
    Glimpse framework

--- NavigationGlimpse/ClientScript.cs
+++ NavigationGlimpse/ClientScript.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Glimpse.Core.Extensibility;
+using Glimpse.Core.Resource;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace NavigationGlimpse
 {
-    class ClientScript
+    public class ClientScript : FileResource, IKey
     {
+        internal const string InternalName = "navigationglimpse_client";
+
+        public ClientScript()
+        {
+            ResourceName = "NavigationGlimpse.navigation.glimpse.js";
+            ResourceType = @"application/x-javascript";
+            Name = InternalName;
+        }
+
+        public string Key
+        {
+            get 
+            { 
+                return Name;
+            }
+        }
     }
 }
--- NavigationGlimpse/ScriptResource.cs
+++ NavigationGlimpse/ScriptResource.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Glimpse.Core.Extensibility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace NavigationGlimpse
 {
-    class ScriptResource
+    public sealed class ScriptResource : IDynamicClientScript
     {
+        public string GetResourceName()
+        {
+            return ClientScript.InternalName;
+        }
+
+        public ScriptOrder Order
+        {
+            get
+            {
+                return ScriptOrder.IncludeAfterClientInterfaceScript;
+            }
+        }
     }
 }
