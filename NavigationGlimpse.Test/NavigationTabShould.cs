63da639eb1ef836ba72ba878ed026306c4739421
Graham Mendick <graham.mendick@gmail.com>
2013-08-24 16:31:06 +0100

    Passed combined dialog.state,transition key

--- NavigationGlimpse.Test/NavigationTabShould.cs
+++ NavigationGlimpse.Test/NavigationTabShould.cs
@@ -21,29 +21,30 @@ namespace NavigationGlimpse.Test
             TransitionElements = elements.Item2;
         }
 
-        private static TransitionElement GetTransition(string dialog, string state, string transition)
+        private static TransitionElement GetTransition(string transitionPath)
         {
-            return TransitionElements.Where(t => t.Transition.Key == transition
-                && t.Transition.Parent.Key == state
-                && t.Transition.Parent.Parent.Key == dialog).First();
+            var keys = transitionPath.Split('.');
+            return TransitionElements.Where(t => t.Transition.Key == keys[2]
+                && t.Transition.Parent.Key == keys[1]
+                && t.Transition.Parent.Parent.Key == keys[0]).First();
         }
 
         [TestMethod]
         public void SetDepthTo0ForSingleTransition()
         {
-            Assert.AreEqual(0, GetTransition("D1", "S1", "T1").Depth);
+            Assert.AreEqual(0, GetTransition("D1.S1.T1").Depth);
         }
 
         [TestMethod]
         public void SetX1To85ForSingleTransitionFromState1()
         {
-            Assert.AreEqual(85, GetTransition("D1", "S1", "T1").X1);
+            Assert.AreEqual(85, GetTransition("D1.S1.T1").X1);
         }
 
         [TestMethod]
         public void SetX2To275ForSingleTransitionToState2()
         {
-            Assert.AreEqual(275, GetTransition("D1", "S1", "T1").X2);
+            Assert.AreEqual(275, GetTransition("D1.S1.T1").X2);
         }
     }
 }
