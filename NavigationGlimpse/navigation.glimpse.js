6ec58937c37c8b14f6c5456a8609873ed6bf11f8
Graham Mendick <graham.mendick@gmail.com>
2013-08-26 13:44:01 +0100

    Moved state text into separate loop
    Avoids repeatedly setting the fillStyle

--- NavigationGlimpse/navigation.glimpse.js
+++ NavigationGlimpse/navigation.glimpse.js
@@ -15,10 +15,12 @@
             var context = navigation.canvas.context;
             for (var i = 0; i < navigation.states.length; i++) {
                 var state = navigation.states[i];
-                context.fillStyle = '#fff';
                 context.rect(state.x, state.y, state.w, state.h);
-                context.fillStyle = '#000';
-                context.font = 'bold 12px Consolas,Courier New';
+            }
+            context.fillStyle = '#000';
+            context.font = 'bold 12px Consolas,Courier New';
+            for (var i = 0; i < navigation.states.length; i++) {
+                var state = navigation.states[i];
                 var shift = Math.max(0, (state.w - context.measureText(state.key).width) / 2);
                 context.fillText(state.key, state.x + shift, state.y + 30, state.w - 2);
             }
