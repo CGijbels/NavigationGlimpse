69d90040633f8f54693bf91e696dd16359e7d01d
Graham Mendick <graham.mendick@gmail.com>
2013-09-02 19:06:39 +0100

    Added StateRouteHandler inspector classes
    Enables access to the page, theme and masters in use for the current
    display mode

---
+++ NavigationGlimpse/StateRouteHandler.cs
@@ -0,0 +1,104 @@
+﻿using Glimpse.Core.Extensibility;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace NavigationGlimpse
+{
+    public class StateRouteHandler : AlternateType<Navigation.StateRouteHandler>
+    {
+        private IEnumerable<IAlternateMethod> allMethods;
+
+        public StateRouteHandler(IProxyFactory proxyFactory)
+            : base(proxyFactory)
+        {
+        }
+
+        public override IEnumerable<IAlternateMethod> AllMethods
+        {
+            get
+            {
+                return allMethods ?? (allMethods = new List<IAlternateMethod>
+                    {
+                        new GetDisplayInfoForPage(),
+                        new GetPageForDisplayInfo(),
+                        new GetDisplayInfoForMaster(),
+                        new GetMasterForDisplayInfo(),
+                        new GetDisplayInfoForTheme(),
+                        new GetThemeForDisplayInfo()
+                    });
+            }
+        }
+
+        public class GetDisplayInfoForPage : AlternateMethod
+        {
+            public GetDisplayInfoForPage()
+                : base(typeof(Navigation.StateRouteHandler), "GetDisplayInfoForPage", BindingFlags.NonPublic | BindingFlags.Instance)
+            {
+            }
+
+            public override void PostImplementation(IAlternateMethodContext context, TimerResult timerResult)
+            {
+            }
+        }
+
+        public class GetPageForDisplayInfo : AlternateMethod
+        {
+            public GetPageForDisplayInfo()
+                : base(typeof(Navigation.StateRouteHandler), "GetPageForDisplayInfo", BindingFlags.NonPublic | BindingFlags.Instance)
+            {
+            }
+
+            public override void PostImplementation(IAlternateMethodContext context, TimerResult timerResult)
+            {
+            }
+        }
+
+        public class GetDisplayInfoForMaster : AlternateMethod
+        {
+            public GetDisplayInfoForMaster()
+                : base(typeof(Navigation.StateRouteHandler), "GetDisplayInfoForMaster", BindingFlags.NonPublic | BindingFlags.Instance)
+            {
+            }
+
+            public override void PostImplementation(IAlternateMethodContext context, TimerResult timerResult)
+            {
+            }
+        }
+
+        public class GetMasterForDisplayInfo : AlternateMethod
+        {
+            public GetMasterForDisplayInfo()
+                : base(typeof(Navigation.StateRouteHandler), "GetMasterForDisplayInfo", BindingFlags.NonPublic | BindingFlags.Instance)
+            {
+            }
+
+            public override void PostImplementation(IAlternateMethodContext context, TimerResult timerResult)
+            {
+            }
+        }
+
+        public class GetDisplayInfoForTheme : AlternateMethod
+        {
+            public GetDisplayInfoForTheme()
+                : base(typeof(Navigation.StateRouteHandler), "GetDisplayInfoForTheme", BindingFlags.NonPublic | BindingFlags.Instance)
+            {
+            }
+
+            public override void PostImplementation(IAlternateMethodContext context, TimerResult timerResult)
+            {
+            }
+        }
+
+        public class GetThemeForDisplayInfo : AlternateMethod
+        {
+            public GetThemeForDisplayInfo()
+                : base(typeof(Navigation.StateRouteHandler), "GetThemeForDisplayInfo", BindingFlags.NonPublic | BindingFlags.Instance)
+            {
+            }
+
+            public override void PostImplementation(IAlternateMethodContext context, TimerResult timerResult)
+            {
+            }
+        }
+    }
+}
---
+++ NavigationGlimpse/StateRouteHandlerInspector.cs
@@ -0,0 +1,34 @@
+﻿using Glimpse.Core.Extensibility;
+using System.Web.Routing;
+
+namespace NavigationGlimpse
+{
+    public class StateRouteHandlerInspector : IInspector
+    {
+        public void Setup(IInspectorContext context)
+        {
+            StateRouteHandler alternateBaseImplementation = new StateRouteHandler(context.ProxyFactory);
+            RouteCollection currentRoutes = RouteTable.Routes;
+            Navigation.StateRouteHandler originalHandler = null;
+            Route stateRoute;
+            using (currentRoutes.GetWriteLock())
+            {
+                foreach (RouteBase route in currentRoutes)
+                {
+                    stateRoute = route as Route;
+                    if (stateRoute != null)
+                    {
+                        originalHandler = stateRoute.RouteHandler as Navigation.StateRouteHandler;
+                        if (originalHandler != null)
+                        {
+                            Navigation.StateRouteHandler newHandler = null;
+                            alternateBaseImplementation.TryCreate(originalHandler, out newHandler, null, new object[] { originalHandler.State });
+                            stateRoute.RouteHandler = newHandler;
+                            context.Logger.Info("");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
