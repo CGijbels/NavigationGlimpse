8841d44bc2033dc045a2ee4b3f768bce1885de4e
Graham Mendick <graham.mendick@gmail.com>
2013-09-22 12:01:26 +0100

    Removed and sorted usings

--- NavigationGlimpse/NavigationTab.cs
+++ NavigationGlimpse/NavigationTab.cs
@@ -2,10 +2,7 @@
 using Glimpse.Core.Extensions;
 using Navigation;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 using System.Web.WebPages;
 
